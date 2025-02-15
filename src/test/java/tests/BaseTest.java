f3df9a06a4265075ce56b6f44884f7ca61dd0098
allatrinda-art <alla.trinda@gmail.com>
2025-02-15 18:17:33 +0300

    additional browser list was added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,30 +2,25 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeMethod;
+import pages.object.enums.Browser;
+
+import static com.codeborne.selenide.Browsers.*;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
     @BeforeMethod
     protected void setUp()    {
-        Configuration.browser = "chrome";
+        Browser browser = Browser.valueOf(System.getProperty("browser", "chrome"));
+        Configuration.browser = switch (browser) {
+            case chrome -> CHROME;
+            case firefox -> FIREFOX;
+            case edge -> EDGE;
+            case safari -> SAFARI;
+        };
         Configuration.pageLoadTimeout = 5000;
         Configuration.timeout = 5000;
         Configuration.browserSize = "1920x1080";
         open("http://litecart.stqa.ru/en/");
-
-//        Browser browser = Browser.valueOf(System.getProperty("browser", "chrome").toLowerCase());
-//        driver = switch(browser) {
-//            case chrome -> {
-//                ChromeOptions options = new ChromeOptions();
-//                options.addArguments("--ignore-certificate-errors");
-//                yield new ChromeDriver();
-//            }
-//            case firefox -> new FirefoxDriver();
-//            case edge -> new EdgeDriver();
-//            case safari -> new SafariDriver();
-//        };
-//        driver.manage().timeouts().getScriptTimeout();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
